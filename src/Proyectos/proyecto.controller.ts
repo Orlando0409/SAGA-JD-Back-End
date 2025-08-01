ff3c99d54eaf03f22dae54049263e434233faa85
Alondra Flores <alondraespinoza897@gmail.com>
2025-08-01 12:41:18 -0600

    Creacion del Module, Controller y service del Modulo de Proyecto

---
+++ src/Proyectos/proyecto.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from "@nestjs/common";
+import { ProyectoService } from "./proyecto.service";
+
+@Controller('proyectos')
+export class ProyectoController
+{
+  constructor(private readonly proyectoService: ProyectoService) {}
+
+  @Get()
+  async AllProyects() 
+  {
+    return this.proyectoService.AllProyects();
+  }
+}
