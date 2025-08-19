798b2112b8d2d41986a5d28fc0b1643c2b403b94
PaolaAlvarez28 <161152689+PaolaAlvarez28@users.noreply.github.com>
2025-08-19 16:17:55 -0600

    cambios

--- src/Modules/Proyectos/ProyectoEntities/EstadoProyecto.Entity.ts
+++ src/Modules/Proyectos/ProyectoEntities/EstadoProyecto.Entity.ts
@@ -1,22 +1,10 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
-<<<<<<< HEAD:src/Modulos/Proyectos/Entidades_proyecto/projectStatus.ts
-<<<<<<< HEAD:src/Modulos/Proyectos/Entidades_proyecto/estado_proyecto.ts
-import { Proyecto_Base } from './entidadBase_proyecto';
-
-@Entity('estado_proyecto')
-export class EstadoProyecto {
-=======
-import { ProjectEntity } from './projectEntity';
-
-=======
 import { ProjectEntity } from './Proyecto.Entity';
->>>>>>> origin/Andres-features:src/Modules/Proyectos/ProyectoEntities/EstadoProyecto.Entity.ts
 
 @Entity('ProjectStatus')
 
 export class ProjectStatus
 {
->>>>>>> Feature/Alondra:src/Modulos/Proyectos/Entidades_proyecto/projectStatus.ts
   @PrimaryGeneratedColumn()
   Id_Estado_Proyecto: number;
 
@@ -26,4 +14,3 @@ export class ProjectStatus
   @OneToMany(() => ProjectEntity, proyecto => proyecto.Estado)
   Proyectos: ProjectEntity[];
 }
->>>>>>> Feature/Alondra:src/Modulos/Proyectos/Entidades_proyecto/projectStatus.ts
--- src/Modules/Proyectos/ProyectoEntities/Proyecto.Entity.ts
+++ src/Modules/Proyectos/ProyectoEntities/Proyecto.Entity.ts
@@ -1,30 +1,10 @@
-<<<<<<< HEAD:src/Modulos/Proyectos/Entidades_proyecto/entidadBase_proyecto.ts
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-  ManyToOne,
-  JoinColumn,
-  UpdateDateColumn,
-} from 'typeorm';
-import { EstadoProyecto } from './estado_proyecto';
-
-@Entity('entidad_base_proyecto')
-export class Proyecto_Base {
-=======
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn,ManyToOne, JoinColumn, UpdateDateColumn  } from 'typeorm';
 import { ProjectStatus } from './EstadoProyecto.Entity';
 
 @Entity('ProjectEntity')   //Se utiliza en la base de datos para crear la tabla
 
 export class ProjectEntity 
-<<<<<<< HEAD:src/Modulos/Proyectos/Entidades_proyecto/projectEntity.ts
-{  
->>>>>>> Feature/Alondra:src/Modulos/Proyectos/Entidades_proyecto/projectEntity.ts
-=======
 {
->>>>>>> origin/Andres-features:src/Modules/Proyectos/ProyectoEntities/Proyecto.Entity.ts
   @PrimaryGeneratedColumn()
   Id_Proyecto: number;
 
@@ -41,7 +21,7 @@ export class ProjectEntity
   Fecha_Actualizacion: Date;
 
   @Column()
-  Id_Usuario: number;
+  Id_Usuario:number;
 
   @ManyToOne(() => ProjectStatus, Estado => Estado.Proyectos)  //Relacion Muchos A uno
   @JoinColumn({ name: 'Id_Estado_Proyecto' })  //LLave Foranea para acceder al estado del proyecto 
@@ -50,4 +30,3 @@ export class ProjectEntity
   @Column()
   ImagenUrl: string;
 }
->>>>>>> origin/Andres-features:src/Modules/Proyectos/ProyectoEntities/Proyecto.Entity.ts
