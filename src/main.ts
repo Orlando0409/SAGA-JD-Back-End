3b079f3a1431ed7955cc90b3e8c49599f45f86d7
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2025-09-09 13:09:58 -0600

    .

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   
   // Configurar CORS correctamente para cookies
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174','http://localhost:5174'], // URLs del frontend
+    origin: ['http://localhost:5173', 'http://localhost:5174'], // URLs del frontend
     credentials: true, //  IMPORTANTE: Permitir cookies
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'cookie']
