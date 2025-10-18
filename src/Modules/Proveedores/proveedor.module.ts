e0723aad65ff5a25e9452e900b70bc091a82c312
Andrés Alvarado <andresalvarado080405@gmail.com>
2025-10-18 03:27:14 -0600

    formateo para proveedores y relacion con usuario

--- src/Modules/Proveedores/proveedor.module.ts
+++ src/Modules/Proveedores/proveedor.module.ts
@@ -4,9 +4,12 @@ import { ProveedorService } from './proveedor.service';
 import { ProveedorController } from './proveedor.controller';
 import { ProveedorFisico, ProveedorJuridico, Proveedor } from './ProveedorEntities/Proveedor.Entity';
 import { EstadoProveedor } from './ProveedorEntities/EstadoProveedor.Entity';
+import { Usuario } from '../Usuarios/UsuarioEntities/Usuario.Entity';
+import { UsuariosModule } from '../Usuarios/Modules/usuarios.module';
+import { AuditoriaModule } from '../Auditoria/auditoria.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Proveedor, ProveedorFisico, ProveedorJuridico, EstadoProveedor])],
+  imports: [TypeOrmModule.forFeature([Proveedor, ProveedorFisico, ProveedorJuridico, EstadoProveedor, Usuario]), UsuariosModule, AuditoriaModule],
   providers: [ProveedorService],
   controllers: [ProveedorController],
   exports: [ProveedorService],
